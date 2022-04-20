8ce9b9b70f7fdf24d02ed050b45e7dadf3ef101c
cesarsiguencia <cesar.siguencia@gmail.com>
2022-04-20 12:19:07 -0400

    deleting console.log statements

--- Develop/sample.js
+++ Develop/sample.js
@@ -15,57 +15,40 @@ setInterval(showTime ,10000)
 
 
 var loadTasks = function(){
-
     var saved = JSON.parse(localStorage.getItem("tasks"))
-    console.log(saved)
     assignments = saved
-    console.log(assignments)
 
     if(!assignments){
         assignments = []
     }
-    console.log(assignments)
 
     assignments.forEach(task => {
-        console.log(task)
-
         var pickedBoxId = task.id
-        console.log(pickedBoxId)
-
         var pickedBoxArea = document.querySelector(".row[data-target='" + pickedBoxId+ "']")
-
         var taskText = pickedBoxArea.querySelector("textarea")
 
         taskText.innerHTML = task.textBox
 
         var time = moment(task.block, "L").set("hour", 17);
-
-        console.log(time)
+        // console.log(time)
         // if(moment(time).isAfter(moment().format('MMMM Do YYYY, 09:00 a'))){
         //     AM9.addClass(".past")
         // }
     })
-
 }
 loadTasks()
 
 var saveTasks = function(event){
-
     var targetEl = event.target
 
     if(targetEl.matches('.saveBtn')){
-    var pickedTextTarget = event.target.getAttribute('data-target')
-    console.log(pickedTextTarget)
+        var pickedBtnId = event.target.getAttribute('data-target')
     }
 
-    var pickedBoxArea = document.querySelector(".row[data-target='" +pickedTextTarget+ "']")
-    console.log(pickedBoxArea)
+    var pickedBoxArea = document.querySelector(".row[data-target='" +pickedBtnId+ "']")
 
     var pickedBoxAreaId = pickedBoxArea.getAttribute("data-target")
-    console.log(pickedBoxAreaId)
-
     var taskText = pickedBoxArea.querySelector("textarea").value
-    console.log(taskText)
     var time = moment().format('MMMM Do YYYY, h:mm a')
 
     var task = {
@@ -73,12 +56,9 @@ var saveTasks = function(event){
             textBox: taskText,
             block: time,
     }
-    console.log(task)
+
     assignments.push(task)
-    console.log(assignments)
-
     localStorage.setItem("tasks", JSON.stringify(assignments))
-
 }
 
 scheduleArea.addEventListener("click", saveTasks);
@@ -89,35 +69,4 @@ scheduleArea.addEventListener("click", saveTasks);
 //     if(moment().isAfter(time)){
 //         timeBlocks.addClass('row')
 //     }
-// }
-
-
-
-
-
-
-// var today = new Date();
-// console.log(today)
-// var day= today.getDay();
-// // var daylist = ["Sunday", "Monday", Turesday",....]
-// var month = today.getMonth();
-// var monthlist =[];
-// var date = today.getDate();
-// var currentTime = today.getHours();
-
-
-
-// for(let i = 9; i < 18, i++ ){
-//     let timeCol = document.getElementById('i');
-//     if (i < currentTime){
-    
-//     } else if (i .currentTime) {
-
-//     } else {
-        
-//     }
-
-// }
-// if (date === 1 || date== 11 || date  === 21 || date === 31){
-//     currentDatEl.textContent= daylist[day] + "," +monthlist[month  + " " +date+ "st";]
 // }
