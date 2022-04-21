24f14481abae0f9c0282bba7c598fb4c4121af0d
cesarsiguencia <cesar.siguencia@gmail.com>
2022-04-21 16:48:28 -0400

    adding color coded blocks depending on past or future

--- Develop/sample.js
+++ Develop/sample.js
@@ -1,17 +1,42 @@
-// add id's as time 1,2,3,4,5 to divs under the calendar
-
 var assignments = []
 var scheduleArea = document.querySelector(".container")
 var timeElement = document.querySelector("#currentDay")
 
-
 var showTime = function(){
-    var time = moment().format('MMMM Do YYYY, h:mm a')
+    var time = moment().format('MMMM Do YYYY, h:mm')
     timeElement.innerHTML = "<p>" + time + "</p>";
 }
 showTime()
 setInterval(showTime ,10000)
 
+var addCSS = function(block, blockTime){
+
+    if (moment().isAfter(blockTime)){
+        $(block).addClass("warning")
+
+    } else if (moment().isBefore(blockTime)){
+        $(block).addClass("future") 
+    } 
+    else {
+        console.log("NOOOOOO", blockTime)
+    }
+}
+
+var grabTimefromBlock = function(){
+
+    var blockCounter = 1
+
+    for (var i = 1; i < 10; i++){
+        var block =""
+        var block = document.querySelector(".col-8[block-hour='hour-" + blockCounter+ "']")
+        var add8Hours = blockCounter + 8
+        var blockTime = moment(add8Hours+":00", "h:mm")
+        blockCounter++
+        addCSS(block, blockTime)
+    }
+ }
+
+grabTimefromBlock()
 
 var loadTasks = function(){
     var saved = JSON.parse(localStorage.getItem("tasks"))
@@ -25,14 +50,7 @@ var loadTasks = function(){
         var pickedBoxId = task.id
         var pickedBoxArea = document.querySelector(".row[data-target='" + pickedBoxId+ "']")
         var taskText = pickedBoxArea.querySelector("textarea")
-
         taskText.innerHTML = task.textBox
-
-        var time = moment(task.block, "L").set("hour", 17);
-        // console.log(time)
-        // if(moment(time).isAfter(moment().format('MMMM Do YYYY, 09:00 a'))){
-        //     AM9.addClass(".past")
-        // }
     })
 }
 loadTasks()
@@ -45,27 +63,15 @@ var saveTasks = function(event){
     }
 
     var pickedBoxArea = document.querySelector(".row[data-target='" +pickedBtnId+ "']")
-
     var pickedBoxAreaId = pickedBoxArea.getAttribute("data-target")
     var taskText = pickedBoxArea.querySelector("textarea").value
-    var time = moment().format('MMMM Do YYYY, h:mm a')
 
     var task = {
             id: pickedBoxAreaId,
             textBox: taskText,
-            block: time,
     }
-
     assignments.push(task)
     localStorage.setItem("tasks", JSON.stringify(assignments))
 }
 
 scheduleArea.addEventListener("click", saveTasks);
-
-
-// var refreshTasks = function(){
-
-//     if(moment().isAfter(time)){
-//         timeBlocks.addClass('row')
-//     }
-// }
